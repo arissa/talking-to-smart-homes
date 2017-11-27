ff05cc65fdaf4af67aa0bb63ab4cfaf5b3dc97a1
arissa@berkeley.edu <arissa@users.noreply.github.com>
2017-11-27 01:32:38 -0800

    removed extraneous files

--- svm.py
+++
@@ -1,8 +0,0 @@
-from sklearn import datasets, svm
-import numpy as np
-features = np.load("features.npy")
-labels = np.load("labels.npy")
-clf = svm.LinearSVC()
-clf.fit(features, labels)
-
-print(clf.predict([features[0]]))
